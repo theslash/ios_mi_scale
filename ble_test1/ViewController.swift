a0dffa1ff2b7c484db12f787459d5e46ad06a9d0
Rudi Krämer <rudi.kraemer@papercuts.de>
2019-12-06 19:30:08 +0100

    Initial Commit

---
+++ ble_test1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ble_test1
+//
+//  Created by Rudi Krämer on 06.12.19.
+//  Copyright © 2019 Rudi Krämer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
